a14786a2ed3fdaaced9c28de80c84c3d1609ff6b
Ilder Velez <velez.ilder@gmail.com>
2021-05-05 21:49:07 -0400

    Fix default image url

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const UserSchema = new mongoose.Schema(
 		avatar: {
 			type: String,
 			default:
-				'https://res.cloudinary.com/drlwtqzgt/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1619727940/samples/animals/cat.jpg',
+				'https://res.cloudinary.com/drlwtqzgt/image/upload/samples/animals/cat.jpg',
 			required: [true, 'Please provide an image url for the avatar.'],
 		},
 		cloudinaryId: {
