802caedc7a916d89faffde97afbff60c46317fe0
Ilder Velez <velez.ilder@gmail.com>
2021-05-07 21:17:24 -0400

    set useFindAndModify to false

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			useFindAndModify: true,
+			useFindAndModify: false,
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true,
