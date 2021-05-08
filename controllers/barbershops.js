49f9c43304ac7826a4807d2f8096601d90095c76
Ilder Velez <velez.ilder@gmail.com>
2021-05-08 02:45:42 -0400

    Add read controllers and create controllers

--- controllers/barbershops.js
+++ controllers/barbershops.js
@@ -1,20 +1,18 @@
 const Barbershop = require('../models/Barbershop')
 const cloudinary = require('../utils/cloudinary')
 
-const getAllBarbershops = async (req, res) => {
+exports.getAllBarbershops = async (req, res) => {
 	try {
 		const barbershops = await Barbershop.find()
-		const barbershopsCount = await Barbershop.find().countDocuments()
+		const barbershopsCount = await Barbershop.countDocuments()
 
-		if (!barbershops) {
-			return res
-				.status(404)
-				.json({ success: false, message: 'No barbershops found.' })
+		if (barbershops) {
+			res
+				.status(200)
+				.json({ success: true, barbershops, count: barbershopsCount })
+		} else {
+			res.status(404).json({ success: false, message: 'No barbershops found.' })
 		}
-
-		res
-			.status(200)
-			.json({ success: true, barbershops, count: barbershopsCount })
 	} catch (error) {
 		console.error(error)
 		res.status(500).json({
@@ -24,19 +22,55 @@ const getAllBarbershops = async (req, res) => {
 	}
 }
 
-const createBarbershop = async (req, res) => {
+exports.getBarbershop = async (req, res) => {
 	try {
-		const { name, address, picture } = req.body
-		const result = await cloudinary.uploader.upload(req.file.path)
+		const { id } = req.params
+		const barbershop = await Barbershop.findById(id)
 
-		const barbershop = {
+		if (barbershop) {
+			res.status(200).json(barbershop)
+		} else {
+			res.status(404).json({ message: 'Barbershop not found.' })
+		}
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ message: 'Server Error' })
+	}
+}
+
+exports.createBarbershop = async (req, res) => {
+	// Lon and lat will come from leaflet
+	try {
+		const {
 			name,
-			address,
-			picture: result.secure_url,
-			cloudinaryId: result.public_id,
+			location: { address, lon, lat },
+			contact: { phoneNumber },
+			available: {
+				hours: { open, close },
+				days,
+			},
+		} = req.body
+
+		const newBarbershop = {
+			name,
+			location: {
+				address,
+				lon,
+				lat,
+			},
+			contact: {
+				phoneNumber,
+			},
+			available: {
+				hours: {
+					open,
+					close,
+				},
+				days,
+			},
 		}
 
-		const created = await Barbershop.create(barbershop)
+		const created = await Barbershop.create(newBarbershop)
 		res.status(200).json({ barbershop: created })
 	} catch (error) {
 		console.error(error)
@@ -47,7 +81,8 @@ const createBarbershop = async (req, res) => {
 	}
 }
 
-module.exports = {
-	getAllBarbershops,
-	createBarbershop,
-}
+// TODO: Update barbershop
+// TODO: Upload avatar
+// TODO: Upload banner
+// TODO: Delete barbershop
+// TODO: Test controllers
