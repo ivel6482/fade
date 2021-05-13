a77b9df14bbb86417c9f27871b394a5375374147
Ilder Velez <velez.ilder@gmail.com>
2021-05-13 01:44:20 -0400

    Rename multer from avatar to image

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const { signup, getLoggedInUser, login } = require('../controllers/auth')
 const { protect } = require('../middleware/auth')
 const upload = require('../utils/multer')
 
-router.route('/signup').post(upload.single('avatar'), signup)
+router.route('/signup').post(upload.single('image'), signup)
 router.route('/login').post(login)
 router.route('/user').get(protect, getLoggedInUser)
 
