2c0f00f5685af3850d027a9230533a3e77c5e00c
Ilder Velez <velez.ilder@gmail.com>
2021-05-31 16:26:54 -0400

    refactor(import): remove Layout import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Signup from './pages/Signup'
-import Layout from './components/Layout'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
