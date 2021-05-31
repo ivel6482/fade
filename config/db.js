350b0b6368d4698efffd11deb43c6226acaf5c4b
Ilder Velez <velez.ilder@gmail.com>
2021-05-31 09:04:22 -0400

    refactor(local-db): add local db for development

--- config/db.js
+++ config/db.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
+	const prodUri = process.env.MONGO_URI_PROD
+	const devUri = process.env.MONGO_URI_DEV
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			useFindAndModify: false,
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		})
+		const conn = await mongoose.connect(
+			process.env.NODE_ENV === 'development' ? devUri : prodUri,
+			{
+				useFindAndModify: false,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true,
+			}
+		)
 
-		console.log(`Database connected`)
+		console.log(`Database connected: ${conn.connection.host}`)
 	} catch (error) {
 		console.log(error)
 		process.exit(1)
