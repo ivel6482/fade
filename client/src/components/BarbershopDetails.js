e1c3a59f0841741bec906034c31193b2b2b6c730
Ilder Velez <velez.ilder@gmail.com>
2021-06-01 20:39:23 -0400

    feat(barbershop-details-style): add banner and profile image styles

--- client/src/components/BarbershopDetails.js
+++ client/src/components/BarbershopDetails.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { BarbershopsContext } from '../store/contexts/barbershopsContext'
 import { useHistory } from 'react-router-dom'
+import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
 
 //TODO: Refactor this component, extract the dashboard layout to a different component.
 
@@ -22,14 +23,60 @@ export default function BarbershopDetails() {
 			{loading ? (
 				<p>Loading barbershop...</p>
 			) : (
-				<section>
-					<h2 className='font-bold text-gray-200 text-7xl'>
-						{barbershop?.name}
-					</h2>
-					<section>
-						<p>{barbershop?.location?.address}</p>
-					</section>
-				</section>
+				<div>
+					<div>
+						<img
+							className='object-cover w-full h-32 rounded-md lg:h-48'
+							src='https://source.unsplash.com/random'
+							alt=''
+						/>
+					</div>
+					<div className='max-w-5xl px-4 mx-auto sm:px-6 lg:px-8'>
+						<div className='-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5'>
+							<div className='flex'>
+								<img
+									className='w-24 h-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32'
+									src='https://source.unsplash.com/random'
+									alt=''
+								/>
+							</div>
+							<div className='mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1'>
+								<div className='flex-1 min-w-0 mt-6 sm:hidden md:block'>
+									<h1 className='text-2xl font-bold text-gray-900 truncate'>
+										{barbershop?.name}
+									</h1>
+								</div>
+								<div className='flex flex-col mt-6 space-y-3 justify-stretch sm:flex-row sm:space-y-0 sm:space-x-4'>
+									<button
+										type='button'
+										className='inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500'
+									>
+										<MailIcon
+											className='w-5 h-5 mr-2 -ml-1 text-gray-400'
+											aria-hidden='true'
+										/>
+										<span>Message</span>
+									</button>
+									<button
+										type='button'
+										className='inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500'
+									>
+										<PhoneIcon
+											className='w-5 h-5 mr-2 -ml-1 text-gray-400'
+											aria-hidden='true'
+										/>
+										<span>Call</span>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className='flex-1 hidden min-w-0 mt-6 sm:block md:hidden'>
+							<h1 className='text-2xl font-bold text-gray-900 truncate'>
+								{barbershop?.name}
+							</h1>
+						</div>
+					</div>
+				</div>
 			)}
 		</section>
 	)
