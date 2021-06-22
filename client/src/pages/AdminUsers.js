c723f11fbced48fe0103719a05a70bb8e90b55b8
Ilder Velez <velez.ilder@gmail.com>
2021-06-22 13:45:29 -0400

    feat(admin-users): add users page for admins

--- client/src/pages/AdminUsers.js
+++ client/src/pages/AdminUsers.js
@@ -1,9 +1,36 @@
 import DashboardLayout from '../components/DashboardLayout'
+import User from '../components/User'
+
 
 export default function AdminUsers() {
+    const users = [
+        {
+            _id: 1,
+            name: 'Machupin1'
+        },
+        {
+            _id: 2,
+            name: 'Machupin2'
+        },
+        {
+            _id: 3,
+            name: 'Machupin3'
+        },
+    ]
     return (
-    <DashboardLayout currentTab='users'>
-            <h2>Admin Users</h2>
+        <DashboardLayout currentTab='users'>
+            <h3 className='font-bold text-gray-300 text-8xl'>Users</h3>
+            <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                {users.length === 0 ? (
+                    <p className='text-gray-500'>
+                        This barbershop currently has no barbers.
+                    </p>
+                ) : (
+                    users.map((user) => {
+                        return <User user={user} key={user._id} />
+                    })
+                )}
+            </ul>
         </DashboardLayout>
     )
 }
