7122a37d2eb62a7978ca79947ada79ccd9e73d6f
Ilder Velez <velez.ilder@gmail.com>
2021-06-24 19:06:23 -0400

    feat(admin-user-state): add user state in admin context

--- client/src/store/contexts/adminContext.js
+++ client/src/store/contexts/adminContext.js
@@ -2,76 +2,103 @@ import axios from 'axios'
 import { createContext, useReducer } from 'react'
 import adminReducer from '../reducers/adminReducer'
 import {
-  GET_USERS_REQUEST,
-  GET_USERS_SUCCESS,
-  GET_USERS_FAIL,
+	GET_USERS_REQUEST,
+	GET_USERS_SUCCESS,
+	GET_USERS_FAIL,
+	GET_USER_REQUEST,
+	GET_USER_SUCCESS,
+	GET_USER_FAIL,
 } from '../actions/adminActions'
 
 const inititalState = {
-  usersCount: 0,
-  users: [],
-  user: null,
-  barbers: [],
-  barber: null,
-  barbershops: [],
-  barbershop: null,
-  appointments: [],
-  appointment: null,
-  loading: false,
-  errors: []
+	usersCount: 0,
+	users: [],
+	user: {},
+	barbersCount: 0,
+	barbers: [],
+	barber: null,
+	barbershopsCount: 0,
+	barbershops: [],
+	barbershop: null,
+	appointmentsCount: 0,
+	appointments: [],
+	appointment: null,
+	loading: false,
+	errors: [],
 }
 
 export const AdminContext = createContext()
 const { Provider } = AdminContext
 
 export const AdminProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(adminReducer, inititalState)
-  const {
-    usersCount,
-    users,
-    user,
-    barbers,
-    barber,
-    barbershops,
-    barbershop,
-    appointments,
-    appointment,
-    loading,
-    errors,
-  } = state
+	const [state, dispatch] = useReducer(adminReducer, inititalState)
+	const {
+		usersCount,
+		users,
+		user,
+		barbersCount,
+		barbers,
+		barber,
+		barbershopsCount,
+		barbershops,
+		barbershop,
+		appointmentsCount,
+		appointments,
+		appointment,
+		loading,
+		errors,
+	} = state
 
-  const getUsers = async () => {
-    try {
-      dispatch({ type: GET_USERS_REQUEST })
-      const res = await axios.get('/users')
-      dispatch({
-        type: GET_USERS_SUCCESS, payload: {
-          count: res.data.count,
-          users: res.data.users,
-        }
-      })
-    } catch (error) {
-      console.error(error)
-      dispatch({ type: GET_USERS_FAIL, payload: error.response.data.message })
-    }
-  }
+	const getUsers = async () => {
+		try {
+			dispatch({ type: GET_USERS_REQUEST })
+			const res = await axios.get('/users')
+			dispatch({
+				type: GET_USERS_SUCCESS,
+				payload: {
+					count: res.data.count,
+					users: res.data.users,
+				},
+			})
+		} catch (error) {
+			console.error(error)
+			dispatch({ type: GET_USERS_FAIL, payload: error.response.data.message })
+		}
+	}
 
-  return (
-    <Provider value={{
-      usersCount,
-      users,
-      user,
-      barbers,
-      barber,
-      barbershops,
-      barbershop,
-      appointments,
-      appointment,
-      loading,
-      errors,
-      getUsers,
-    }}>
-      {children}
-    </Provider>
-  )
+	const getUser = async (id) => {
+		try {
+			dispatch({ type: GET_USER_REQUEST })
+			const res = await axios.get(`/users/${id}`)
+			console.log('RAAAAAAAAAAAAAN')
+			dispatch({ type: GET_USER_SUCCESS, payload: res.data })
+		} catch (error) {
+			console.error(error)
+			dispatch({ type: GET_USER_FAIL, payload: error.response.data.message })
+		}
+	}
+	return (
+		<Provider
+			value={{
+				usersCount,
+				users,
+				user,
+				barbersCount,
+				barbers,
+				barber,
+				barbershopsCount,
+				barbershops,
+				barbershop,
+				appointmentsCount,
+				appointments,
+				appointment,
+				loading,
+				errors,
+				getUsers,
+				getUser,
+			}}
+		>
+			{children}
+		</Provider>
+	)
 }
--- client/src/store/reducers/adminReducer.js
+++ client/src/store/reducers/adminReducer.js
@@ -1,37 +1,56 @@
 import {
-  GET_USERS_REQUEST,
-  GET_USERS_SUCCESS,
-  GET_USERS_FAIL,
+	GET_USERS_REQUEST,
+	GET_USERS_SUCCESS,
+	GET_USERS_FAIL,
+	GET_USER_REQUEST,
+	GET_USER_SUCCESS,
+	GET_USER_FAIL,
 } from '../actions/adminActions'
 
 export default function adminReducer(state, action) {
-  const { type, payload } = action
+	const { type, payload } = action
 
-  switch (type) {
-    case GET_USERS_REQUEST:
-      return {
-        ...state,
-        loading: true,
-      }
+	switch (type) {
+		case GET_USERS_REQUEST:
+		case GET_USER_REQUEST:
+			return {
+				...state,
+				loading: true,
+			}
 
-    case GET_USERS_SUCCESS:
-      return {
-        ...state,
-        usersCount: payload.count,
-        users: payload.users,
-        loading: false,
-      }
+		case GET_USERS_SUCCESS:
+			return {
+				...state,
+				usersCount: payload.count,
+				users: payload.users,
+				loading: false,
+			}
 
-    case GET_USERS_FAIL:
-      return {
-        ...state,
-        usersCount: 0,
-        users: [],
-        loading: false,
-        errors: [...state.errors, payload]
-      }
+		case GET_USER_SUCCESS:
+			return {
+				...state,
+				user: payload,
+				loading: false,
+			}
 
-    default:
-      return state
-  }
+		case GET_USERS_FAIL:
+			return {
+				...state,
+				usersCount: 0,
+				users: [],
+				loading: false,
+				errors: [...state.errors, payload],
+			}
+
+		case GET_USER_FAIL:
+			return {
+				...state,
+				user: null,
+				loading: false,
+				errors: [...state.errors, payload],
+			}
+
+		default:
+			return state
+	}
 }
