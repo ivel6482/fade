9944a104826103b881578cd6759b5c258f8b2907
Ilder Velez <velez.ilder@gmail.com>
2021-06-29 18:18:03 -0400

    feat(barbershop-model): add about field and capitalized days enum values to be in sync with frontend

--- models/Barbershop.js
+++ models/Barbershop.js
@@ -1,86 +1,90 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const BarbershopSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please enter a barbershop name."],
-      trim: true,
-    },
-    location: {
-      address: {
-        type: String,
-        required: [true, "Please enter a barbershop address"],
-        trim: true,
-      },
-      lon: {
-        type: String,
-      },
-      lat: {
-        type: String,
-      },
-    },
-    banner: {
-      url: {
-        type: String,
-      },
-      cloudinaryId: {
-        type: String,
-      },
-    },
-    avatar: {
-      url: {
-        // TODO: Set a default.
-        type: String,
-      },
-      cloudinaryId: {
-        type: String,
-      },
-    },
-    contact: {
-      phoneNumber: {
-        type: String,
-        required: [true, "Please add a phone number, instead got {VALUE}"],
-        match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-      },
-    },
-    available: {
-      hours: {
-        open: {
-          type: String,
-        },
-        close: {
-          type: String,
-        },
-      },
-      days: {
-        type: [String],
-        enum: [
-          "sunday",
-          "monday",
-          "tuesday",
-          "wednesday",
-          "thursday",
-          "friday",
-          "saturday",
-        ],
-      },
-    },
-    favoriteCount: {
-      type: Number,
-      default: 0,
-    },
-    favorites: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "User",
-    },
+	{
+		name: {
+			type: String,
+			required: [true, 'Please enter a barbershop name.'],
+			trim: true,
+		},
+		about: {
+			type: String,
+			trim: true,
+		},
+		location: {
+			address: {
+				type: String,
+				required: [true, 'Please enter a barbershop address'],
+				trim: true,
+			},
+			lon: {
+				type: String,
+			},
+			lat: {
+				type: String,
+			},
+		},
+		banner: {
+			url: {
+				type: String,
+			},
+			cloudinaryId: {
+				type: String,
+			},
+		},
+		avatar: {
+			url: {
+				// TODO: Set a default.
+				type: String,
+			},
+			cloudinaryId: {
+				type: String,
+			},
+		},
+		contact: {
+			phoneNumber: {
+				type: String,
+				required: [true, 'Please add a phone number, instead got {VALUE}'],
+				match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+			},
+		},
+		available: {
+			hours: {
+				open: {
+					type: String,
+				},
+				close: {
+					type: String,
+				},
+			},
+			days: {
+				type: [String],
+				enum: [
+					'Sunday',
+					'Monday',
+					'Tuesday',
+					'Wednesday',
+					'Thursday',
+					'Friday',
+					'Saturday',
+				],
+			},
+		},
+		favoriteCount: {
+			type: Number,
+			default: 0,
+		},
+		favorites: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: 'User',
+		},
 
-    // owner: {
-    // 	type: mongoose.Schema.Types.ObjectId,
-    // 	ref: 'Owner'
-    // },
-  },
-  { timestamps: true }
+		// owner: {
+		// 	type: mongoose.Schema.Types.ObjectId,
+		// 	ref: 'Owner'
+		// },
+	},
+	{ timestamps: true }
 )
 
-module.exports = mongoose.model("Barbershop", BarbershopSchema)
+module.exports = mongoose.model('Barbershop', BarbershopSchema)
