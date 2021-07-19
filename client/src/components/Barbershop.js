8ead616ca0113baa6a76d26317638fcc62cd6b16
Ilder Velez <velez.ilder@gmail.com>
2021-07-19 16:38:19 -0400

    refactor(favorites): remove favorites feature

--- client/src/components/Barbershop.js
+++ client/src/components/Barbershop.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import {
 	PhoneIcon,
-	HeartIcon,
 	LocationMarkerIcon,
 	ArrowRightIcon,
 } from '@heroicons/react/solid'
@@ -17,12 +16,8 @@ export default function Barbershop({ barbershop }) {
 	return (
 		<section className='col-span-1 p-4'>
 			<section className='p-4 space-y-2 text-gray-700 transition bg-gray-100 border border-gray-100 rounded-md shadow hover:shadow-md hover:border-gray-300'>
-				<section className='flex justify-between'>
+				<section>
 					<p className='font-semibold text-gray-900'>{barbershop?.name}</p>
-					<button className='flex items-center space-x-1 font-semibold transition hover:text-red-500'>
-						<span>{barbershop.favoriteCount}</span>{' '}
-						<HeartIcon width='20' className='' />
-					</button>
 				</section>
 				<BarbershopItem
 					icon={<LocationMarkerIcon width='20' className='text-gray-400' />}
