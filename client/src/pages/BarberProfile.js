3c973de8d40b77d8a3e954f801022bed8216df90
Ilder Velez <velez.ilder@gmail.com>
2021-07-22 16:02:44 -0400

    refactor(styles): change button colors

--- client/src/pages/BarberProfile.js
+++ client/src/pages/BarberProfile.js
@@ -95,13 +95,13 @@ export default function BarberProfile() {
 					<div className='flex justify-end'>
 						<Link
 							to='/dashboard'
-							className='px-4 py-2 text-sm font-semibold text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+							className='px-4 py-2 text-sm font-semibold text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
 						>
 							Cancel
 						</Link>
 						<button
 							type='submit'
-							className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-semibold text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+							className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-semibold text-gray-200 bg-blue-900 border border-transparent rounded-md shadow-sm hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900'
 						>
 							Save
 						</button>
