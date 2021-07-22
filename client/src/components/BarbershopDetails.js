dc8c31c4ec6e6ca1b12e07aa96117ece0e6d5226
Ilder Velez <velez.ilder@gmail.com>
2021-07-22 15:35:49 -0400

    fix(barbershop-barbers): improve grid columns reponsiveness

--- client/src/components/BarbershopDetails.js
+++ client/src/components/BarbershopDetails.js
@@ -135,7 +135,7 @@ export default function BarbershopDetails() {
 						This barbershop currently has no barbers.
 					</p>
 				) : (
-					<ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+					<ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-4'>
 						{barbers.map((barber) => (
 							<Barber barber={barber} key={barber._id} />
 						))}
