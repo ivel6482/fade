8a3ddd05083d5b7e8742d0fb17299786c79524c5
Ilder Velez <velez.ilder@gmail.com>
2021-07-22 14:50:58 -0400

    refactor(barber): change book appointment background color on hover

--- client/src/components/Barber.js
+++ client/src/components/Barber.js
@@ -15,7 +15,7 @@ export default function Barber({ barber }) {
 	return (
 		<>
 			{open && <Modal open={open} setOpen={setOpen} />}
-			<li className='flex flex-col col-span-1 text-center bg-white divide-y divide-gray-200 rounded-lg shadow'>
+			<li className='flex flex-col col-span-1 overflow-hidden text-center bg-white divide-y divide-gray-200 rounded-lg shadow'>
 				<div className='flex flex-col flex-1 p-8'>
 					<img
 						className='flex-shrink-0 w-32 h-32 mx-auto bg-black rounded-full'
@@ -30,30 +30,19 @@ export default function Barber({ barber }) {
 						<dd className='text-sm text-gray-500'>{barber.title}</dd>
 						<dt className='sr-only'>Role</dt>
 						<dd className='mt-3'>
-							{/* {barber.specialties.map((specialty, index) => (
-								//TODO: use uuid for key instead of index, implement this in the database model and controller.
-								<span
-									key={index}
-									className='px-2 py-1 text-xs font-medium text-green-800 bg-green-100 rounded-full'
-								>
-									{specialty}
-								</span>
-							))} */}
+							{/* //TODO: use uuid for key instead of index, implement this in the database model and controller. */}
 						</dd>
 					</dl>
 				</div>
 				<div>
 					<div className='flex -mt-px divide-x divide-gray-200'>
 						{/* //TODO: When this is clicked show a modal with the appointments. */}
-						<div className='flex flex-1 w-0 transition hover:bg-gray-50'>
+						<div className='flex flex-1 w-0 transition hover:bg-blue-900 hover:text-gray-200'>
 							<button
 								onClick={() => fetchBarberAppointments(barber._id)}
-								className='relative inline-flex items-center justify-center flex-1 w-0 py-4 -mr-px text-sm font-medium text-gray-700 border border-transparent rounded-bl-lg hover:text-gray-500'
+								className='relative inline-flex items-center justify-center flex-1 w-0 py-4 -mr-px text-sm font-medium border border-transparent rounded-bl-lg '
 							>
-								<CalendarIcon
-									className='w-5 h-5 text-gray-400'
-									aria-hidden='true'
-								/>
+								<CalendarIcon className='w-5 h-5' aria-hidden='true' />
 								<span className='ml-3'>Book Appointment</span>
 							</button>
 						</div>
