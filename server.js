4f582d5afb167f806eba6f37e262d2b2cb77390a
Ilder Velez <velez.ilder@gmail.com>
2021-07-22 22:14:04 -0400

    build(heroku): setup deployment

--- server.js
+++ server.js
@@ -26,12 +26,10 @@ app.use('/api/v1/barbershops', barbershopRoutes)
 app.use('/api/v1/barbers', barberRoutes)
 app.use('/api/v1/appointments', appointmentRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'))
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
-}
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
