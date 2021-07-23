bd0ee291043ab0b8f327290ca3a0c84e18f429f1
Ilder Velez <velez.ilder@gmail.com>
2021-07-23 15:30:24 -0400

    build(heroku): setup deployment

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ connectDB()
 
 app.use(morgan('dev'))
 
-app.use(cors())
+// app.use(cors())
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/users', usersRoutes)
 app.use('/api/v1/barbershops', barbershopRoutes)
