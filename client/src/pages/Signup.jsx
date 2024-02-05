e469576cbfa0862e1130c2fb8e51d2a1877b1cc8
Ilder Vélez <38200521+ivel6482@users.noreply.github.com>
2024-02-04 22:34:47 -0400

    implement react-hook-form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -4,13 +4,19 @@ import { UserContext } from '../store/contexts/userContext'
 import { NotificationContext } from '../store/contexts/notificationsContext'
 import { Layout } from '../components/Layout'
 import { TicketIcon } from '@heroicons/react/24/outline'
+import { useForm } from "react-hook-form";
+import { Input } from "../components/Form/Input"
 
 export const Signup = () => {
-	const [firstName, setFirstName] = useState('')
-	const [lastName, setLastName] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [confirmPassword, setConfirmPassword] = useState('')
+	const { register, handleSubmit, reset, formState: { errors } } = useForm({
+		defaultValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			password: '',
+			confirmPassword: ''
+		}
+	});
 
 	const { signup, loading, user, isAuthenticated } = useContext(UserContext)
 	const { displayNotification } = useContext(NotificationContext)
@@ -36,7 +42,6 @@ export const Signup = () => {
 		setLastName('')
 		setPassword('')
 	}
-	// FIXME: Add a confirm password field
 	return (
 		<Layout>
 			<div className='w-full max-w-sm mx-auto lg:w-96'>
@@ -58,111 +63,23 @@ export const Signup = () => {
 
 				<div className='mt-8'>
 					<div className='mt-6'>
-						<form onSubmit={submitHandler} className='space-y-6'>
-							<div>
-								<label
-									htmlFor='firstName'
-									className='block text-sm font-medium text-gray-700'
-								>
-									First Name
-								</label>
-								<div className='mt-1'>
-									<input
-										id='firstName'
-										name='firstName'
-										type='firstName'
-										required
-										className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-1 focus:ring-blue-700 focus:border-blue-700 sm:text-sm'
-										onChange={(e) => setFirstName(e.target.value)}
-									/>
-								</div>
-							</div>
-							<div>
-								<label
-									htmlFor='lastName'
-									className='block text-sm font-medium text-gray-700'
-								>
-									Last Name
-								</label>
-								<div className='mt-1'>
-									<input
-										id='lastName'
-										name='lastName'
-										type='lastName'
-										required
-										className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm focus:ring-1'
-										onChange={(e) => setLastName(e.target.value)}
-									/>
-								</div>
-							</div>
-							<div>
-								<label
-									htmlFor='email'
-									className='block text-sm font-medium text-gray-700'
-								>
-									Email address
-								</label>
-								<div className='mt-1'>
-									<input
-										id='email'
-										name='email'
-										type='email'
-										autoComplete='email'
-										required
-										onChange={(e) => setEmail(e.target.value)}
-										value={email}
-										className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm'
-									/>
-								</div>
-							</div>
-
-							<div className='space-y-1'>
-								<label
-									htmlFor='password'
-									className='block text-sm font-medium text-gray-700'
-								>
-									Password
-								</label>
-								<div className='mt-1'>
-									<input
-										id='password'
-										name='password'
-										type='password'
-										autoComplete='current-password'
-										required
-										onChange={(e) => setPassword(e.target.value)}
-										value={password}
-										className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm'
-									/>
-								</div>
-							</div>
-
-							<div className='space-y-1'>
-								<label
-									htmlFor='confirm-password'
-									className='block text-sm font-medium text-gray-700'
-								>
-									Confirm Password
-								</label>
-								<div className='mt-1'>
-									<input
-										id='confirm-password'
-										name='confirm-password'
-										type='password'
-										autoComplete='current-password'
-										required
-										onChange={(e) => setConfirmPassword(e.target.value)}
-										value={confirmPassword}
-										className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-700 focus:border-blue-700 sm:text-sm'
-									/>
-								</div>
-							</div>
+						<form onSubmit={handleSubmit(submitHandler)} className='space-y-6'>
+							<Input label="First name" name="firstName" required={true} register={register} />
+							<Input label="Last name" name="lastName" required={true} register={register} />
+							<Input label="Email" name="email" required={true} register={register} />
+							<Input type="password" label="Password" name="password" required={true} register={register} />
+							<Input type="password" label="Confirm password" name="confirmPassword" required={true} register={register} validate={{
+								samePassword: (confirmPassword, formData) => {
+									return confirmPassword === formData.password || "Passwords should match."
+								}
+							}}
+								errors={errors} />
 							<div>
 								<button
 									type='submit'
 									className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-900 border border-transparent rounded-md shadow-sm hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900'
 								>
-									{loading ? 'Authenticating...' : 'Sign Up'}
+									{loading ? 'Authenticating...' : 'Sign up'}
 								</button>
 							</div>
 						</form>
