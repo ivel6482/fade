49bc49105efb5fdcf24ca7782cc843add3f240a7
Ilder Vélez <38200521+ivel6482@users.noreply.github.com>
2024-02-27 22:03:52 -0400

    move days and hours to their own files

--- client/src/pages/AdminBarbershop.jsx
+++ client/src/pages/AdminBarbershop.jsx
@@ -13,43 +13,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Checkbox } from '../components/Form/Checkbox'
 import { SelectInput } from '../components/Form/SelectInput'
 import { useUpdateBarbershop, useDeleteBarbershop } from "../mutations/barbershopMutations";
-
-const days = [
-	'Monday',
-	'Tuesday',
-	'Wednesday',
-	'Thursday',
-	'Friday',
-	'Saturday',
-	'Sunday',
-];
-
-const hours = [
-	'12:00 AM',
-	'1:00 AM',
-	'2:00 AM',
-	'3:00 AM',
-	'4:00 AM',
-	'5:00 AM',
-	'6:00 AM',
-	'7:00 AM',
-	'8:00 AM',
-	'9:00 AM',
-	'10:00 AM',
-	'11:00 AM',
-	'12:00 PM',
-	'1:00 PM',
-	'2:00 PM',
-	'3:00 PM',
-	'4:00 PM',
-	'5:00 PM',
-	'6:00 PM',
-	'7:00 PM',
-	'8:00 PM',
-	'9:00 PM',
-	'10:00 PM',
-	'11:00 PM',
-];
+import { days } from '../utils/days';
+import { hours } from '../utils/hours';
 
 const hoursOptions = hours.map(hour => ({ text: hour, value: hour }));
 
