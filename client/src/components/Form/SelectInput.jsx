fee723e6c09f6f042ad06383378032d983bb620f
Ilder Vélez <38200521+ivel6482@users.noreply.github.com>
2024-02-27 23:16:38 -0400

    fix SelectInput default values

--- client/src/components/Form/SelectInput.jsx
+++ client/src/components/Form/SelectInput.jsx
@@ -9,7 +9,7 @@ export const SelectInput = ({ id, name, register, errors, defaultOptionValue = "
         className='block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-blue-700 focus:border-blue-700 sm:max-w-xs sm:text-sm'
         {...register(name)}
       >
-        <option value=''>Select open time</option>
+        <option value={defaultOptionValue}>{defaultOptionText}</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.text}
